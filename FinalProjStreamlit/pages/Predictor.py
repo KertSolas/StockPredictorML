8f4e9edf8b1a59cc82e563db4cb7e14d22a66cb5
KewlWhip <78395423+KertSolas@users.noreply.github.com>
2023-12-21 13:40:59 +0800

    Update Predictor.py

--- FinalProjStreamlit/pages/Predictor.py
+++ FinalProjStreamlit/pages/Predictor.py
@@ -30,7 +30,7 @@ def selectBox():
     return selectionBox
 
 def trainModel(data):
-    df_train = data[["Date", "Close"]]
+    df_train = data[["Date", "Close", "Volume"]]
     df_train = data.rename(columns={"Date": "ds", "Close": "y"})
     model = Prophet()
     model.fit(df_train) 
@@ -39,21 +39,24 @@ def trainModel(data):
 def forecast(model):
     future = model.make_future_dataframe(periods= period)
     forecast = model.predict(future)
+
     fig = plot_plotly(model, forecast)
     st.plotly_chart(fig)
 
-def displayData():
+def displayData(data):
     fig = go.Figure()
-    fig.add_trace(go.Scatter(x=getStockData('AAPL', '2019-01-01', '2020-01-01')['Date'], y=getStockData('AAPL', '2019-01-01', '2020-01-01')['Close'], name="stock_open"))
-    fig.layout.update(title_text="Time Series Data", xaxis_rangeslider_visible=True)   
+    fig.add_trace(go.Scatter(x=data['Date'], y=data['Close'], name="stock_open"))
+    fig.layout.update(xaxis_rangeslider_visible=True)   
     st.plotly_chart(fig) 
+
 
 
 def main():
     selectionBox = selectBox()
     data = getStockData(selectionBox, "2015-01-01", today)
+    st.subheader(selectionBox + " Data")
     st.write(data.tail())
-    displayData() #raw data
+    displayData(data) #raw data
 
     train = trainModel(data)
     st.subheader("Forecast Data")
