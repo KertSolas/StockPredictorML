e6432a0eb9830e9bd061630d6b87802fbe9e040f
KewlWhip <78395423+KertSolas@users.noreply.github.com>
2023-12-21 10:53:18 +0800

    Update Predictor.py

--- FinalProjStreamlit/Predictor.py
+++ FinalProjStreamlit/Predictor.py
@@ -2,17 +2,62 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import yfinance as yf
+from datetime import date
+from prophet import Prophet
+from prophet.plot import plot_plotly
+from plotly import graph_objs as go
 
+# DONT FORGOR UPDATE DATE SELECTION
+# 
 
+stocks = ("AAPL", "GOOG", "MSFT", "GME", "AMC")
+selectionBox = st.selectbox("Select a stock", stocks)
+today = date.today()
+startYear = today.year
+startYear = 0
+endYear = startYear + 5
+
+year = st.slider("Select a year", startYear, endYear)
+period = year * 365
+
+@st.cache_data
 def getStockData(symbol, start_date, end_date):
     data = pd.DataFrame(yf.download(symbol, start=start_date, end=end_date))
+    data.reset_index(inplace=True)
     return data
 
-def displayData():
-    return st.line_chart(getStockData('AAPL', '2019-01-01', '2020-01-01')[["Close", "Open"]])
+def trainModel(data):
+    df_train = data[["Date", "Close"]]
+    df_train = data.rename(columns={"Date": "ds", "Close": "y"})
+    model = Prophet()
+    model.fit(df_train) 
+    return model
 
-st.write(getStockData('AAPL', '2019-01-01', '2020-01-01'))
-displayData()
+def forecast(model):
+    future = model.make_future_dataframe(periods= period)
+    forecast = model.predict(future)
+    fig = plot_plotly(model, forecast)
+    st.plotly_chart(fig)
+
+def displayData():
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=getStockData('AAPL', '2019-01-01', '2020-01-01')['Date'], y=getStockData('AAPL', '2019-01-01', '2020-01-01')['Close'], name="stock_open"))
+    fig.layout.update(title_text="Time Series Data", xaxis_rangeslider_visible=True)   
+    st.plotly_chart(fig) 
+
+
+def main():
+    data = getStockData(selectionBox, "2015-01-01", today)
+    st.write(data.tail())
+    displayData() #raw data
+
+    train = trainModel(data)
+    st.subheader("Forecast Data")
+    forecastData = forecast(train)
+
+main()
+
+
 
 
 
