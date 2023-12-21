de006ef597b6e98e83b213eec7b647bb73b7046d
SeaJayL <cjwatch69@gmail.com>
2023-12-21 21:31:42 +0800

    More updates for KNN

--- FinalProjStreamlit/pages/KNN.py
+++ FinalProjStreamlit/pages/KNN.py
@@ -6,13 +6,43 @@ from sklearn.metrics import accuracy_score
 import yfinance as yf
 import matplotlib.pyplot as plt
 import numpy as np
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import classification_report
 
 def get_stock_data(ticker, start_date, end_date):
     stock_data = yf.download(ticker, start=start_date, end=end_date)
     return stock_data
 
-def train_knn(X, y, n_neighbors=5):
-    model = KNeighborsClassifier(n_neighbors=n_neighbors)
-    model.fit(X, y)
-    return model
+data = get_stock_data('AAPL', '2020-01-01', '2022-12-31')
 
+X = data.drop('target_column', axis=1) 
+y = data['target_column']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Standardize the features using StandardScaler
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+
+# Create and train the KNN model
+k = 3
+knn_model = KNeighborsClassifier(n_neighbors=k)
+knn_model.fit(X_train_scaled, y_train)
+
+# Make predictions on the test set
+y_pred = knn_model.predict(X_test_scaled)
+
+# Visualize the regression results
+plt.scatter(y_test, y_pred)
+plt.xlabel("Actual Values")
+plt.ylabel("Predicted Values")
+plt.title("Actual vs Predicted Values")
+plt.show()
+
+# Evaluate the model
+accuracy = accuracy_score(y_test, y_pred)
+print(f"Accuracy: {accuracy:.2f}")
+
+# Display classification report
+print("Classification Report:\n", classification_report(y_test, y_pred))
