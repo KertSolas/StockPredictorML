43fbbea02bd431c010c5a73b1c45e63a2f3e7640
SeaJayL <cjwatch69@gmail.com>
2023-12-22 08:33:47 +0800

    more fixes

--- FinalProjStreamlit/pages/KNN.py
+++ FinalProjStreamlit/pages/KNN.py
@@ -21,17 +21,14 @@ y = data['Volume']
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Standardize the features using StandardScaler
 scaler = StandardScaler()
 X_train_scaled = scaler.fit_transform(X_train)
 X_test_scaled = scaler.transform(X_test)
 
-# Create and train the KNN model
 k = 3
 knn_model = KNeighborsClassifier(n_neighbors=k)
 knn_model.fit(X_train_scaled, y_train)
 
-# Make predictions on the test set
 y_pred = knn_model.predict(X_test_scaled)
 
 # Visualize the regression results
@@ -41,9 +38,7 @@ plt.ylabel("Predicted Values")
 plt.title("Actual vs Predicted Values")
 plt.show()
 
-# Evaluate the model
 accuracy = accuracy_score(y_test, y_pred)
 print(f"Accuracy: {accuracy:.2f}")
 
-# Display classification report
 print("Classification Report:\n", classification_report(y_test, y_pred))
