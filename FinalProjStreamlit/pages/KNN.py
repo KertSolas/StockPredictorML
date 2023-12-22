699c5eada3a5f39e0a7715b0453ba8be722edd14
SeaJayL <cjwatch69@gmail.com>
2023-12-22 08:47:13 +0800

    Added UI for KNN

--- FinalProjStreamlit/pages/KNN.py
+++ FinalProjStreamlit/pages/KNN.py
@@ -1,44 +1,57 @@
 import streamlit as st
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.metrics import accuracy_score
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.metrics import mean_squared_error
 import yfinance as yf
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.preprocessing import StandardScaler
-from sklearn.metrics import classification_report
 import plotly.express as px
 
-def get_stock_data(ticker, start_date, end_date):
+def stock_data(ticker, start_date, end_date):
     stock_data = yf.download(ticker, start=start_date, end=end_date)
     return stock_data
 
-data = get_stock_data('AAPL', '2020-01-01', '2022-12-31')
+def predict_knn(data):
+    data['Date'] = data.index
+    data['Date'] = data['Date'].astype(np.int64) // 10**9  
+    X = data[['Date']]
+    y = data['Close']
+    return X, y
 
-X = data.drop('Open', axis=1) 
-y = data['Volume']
+def main():
+    st.title("Stock Predictor App")
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    ticker = st.text_input("Enter Stock Ticker (e.g., AAPL):", "AAPL")
+    start_date = st.date_input("Select Start Date:", pd.to_datetime('2022-01-01'))
+    end_date = st.date_input("Select End Date:", pd.to_datetime('2023-01-01'))
 
-scaler = StandardScaler()
-X_train_scaled = scaler.fit_transform(X_train)
-X_test_scaled = scaler.transform(X_test)
+    stock_data_value = stock_data(ticker, start_date, end_date)
+    X, y = predict_knn(stock_data_value)
 
-k = 3
-knn_model = KNeighborsClassifier(n_neighbors=k)
-knn_model.fit(X_train_scaled, y_train)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-y_pred = knn_model.predict(X_test_scaled)
+    scaler = StandardScaler()
+    X_train_scaled = scaler.fit_transform(X_train)
+    X_test_scaled = scaler.transform(X_test)
 
-# Visualize the regression results
-plt.scatter(y_test, y_pred)
-plt.xlabel("Actual Values")
-plt.ylabel("Predicted Values")
-plt.title("Actual vs Predicted Values")
-plt.show()
+    k = 3
+    knn_model = KNeighborsRegressor(n_neighbors=k)
+    knn_model.fit(X_train_scaled, y_train)
 
-accuracy = accuracy_score(y_test, y_pred)
-print(f"Accuracy: {accuracy:.2f}")
+    y_pred = knn_model.predict(X_test_scaled)
 
-print("Classification Report:\n", classification_report(y_test, y_pred))
+    st.subheader(f"{ticker} Stock Data")
+    st.write(stock_data_value)
+
+    st.subheader("Regression Plot")
+    fig = px.scatter(x=y_test, y=y_pred, labels={'x': 'Actual Values', 'y': 'Predicted Values'},
+                     title="Actual vs Predicted Values")
+    st.plotly_chart(fig)
+
+    st.subheader("Model Evaluation")
+    mse = mean_squared_error(y_test, y_pred)
+    st.write(f"Mean Squared Error: {mse:.2f}")
+
+main()
